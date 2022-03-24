db360bfd930de42f9ad4e27f03209bbc4a2d70e0
unknown <pattayiljithin@gmail.com>
2022-03-24 09:09:19 +0530

    delete

--- src/ApolloClient.js
+++
@@ -1,9 +0,0 @@
-import ApolloClient from "apollo-boost";
-import config from './client-config';
-
-// apollo client setup
-const client = new ApolloClient({
-	uri: config.graphqlUrl
-});
-
-export default client;
--- src/components/Loading.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Loading = () => {
-	return <p className="container">Loading Data...</p>;
-}
-
-export default Loading;
