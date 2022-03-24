1d702d1e1f73d517721b3ba6a394152d93be8617
prajital <95456971+prajital@users.noreply.github.com>
2022-03-24 10:40:27 +0530

    client-config

---
+++ src/client-config.js
@@ -0,0 +1,10 @@
+/**
+ * Contains client side configuration options.
+ */
+
+const config = {
+	siteUrl: 'https://cms.talkies.online',
+	graphqlUrl: 'https://cms.talkies.online/graphql',
+};
+
+export default config;
